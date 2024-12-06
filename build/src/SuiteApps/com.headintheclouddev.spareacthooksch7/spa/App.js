57a64166041755e7a872efbd7ffeb44a3b081545
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2024-12-06 10:37:53 +0000

    App 7.1: Replaced content with pages, testing PostPage

--- build/src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/App.js
+++ build/src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/App.js
@@ -1,39 +1,17 @@
 import { jsx as _jsx, jsxs as _jsxs } from "@uif-js/core/jsx-runtime";
 import { useReducer, useEffect, VDom, useState } from '@uif-js/core';
-// import UserBar from './user/UserBar';
-import PostList from './post/PostList';
-// import CreatePost from './post/CreatePost';
+// import PostList from './post/PostList';
 import appReducer from "./reducers";
-// import Header from './Header';
-// import ChangeTheme from './ChangeTheme';
 import { ThemeContext } from './contexts';
 import log from 'N/log';
-import query from "N/query";
 import HeaderBar from "./pages/HeaderBar";
-// const defaultPosts: IPostProps[] = [
-//   { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
-//   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
-// ];
+// import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
 export default function App() {
     log.debug('App', `Initializing at ${new Date()}`);
     const [theme, setTheme] = useState({ primaryColor: 'deepskyblue', secondaryColor: 'coral' });
     const [state, dispatch] = useReducer(appReducer, { user: '', posts: [], error: '' });
     const { user, posts, error } = state;
-    const getPosts = () => {
-        query.runSuiteQL.promise({ query: `SELECT id, name AS title, custrecord_blog_content AS content, BUILTIN.DF(owner) AS author FROM customrecord_blog_post` }).then((resultSet) => {
-            const results = resultSet.asMappedResults();
-            dispatch({ type: 'FETCH_POSTS', posts: results });
-        });
-    };
-    useEffect(getPosts, []);
-    useEffect(() => {
-        if (posts && posts.error) {
-            dispatch({ type: 'POSTS_ERROR' });
-        }
-        if (posts && posts.data) {
-            dispatch({ type: 'FETCH_POSTS', posts: posts.data.reverse() });
-        }
-    }, [posts]);
     useEffect(() => {
         if (user) {
             document.title = `${user} - React Hooks Blog`;
@@ -46,5 +24,6 @@ export default function App() {
         // [StateContext]: { state, dispatch }, // This doesn't seem to work
         [ThemeContext]: theme
     };
-    return (_jsx(VDom.Context, { value: context, children: _jsxs("div", { style: { padding: 8 }, children: [_jsx(HeaderBar, { user: user, posts: posts, postDispatch: dispatch, setTheme: setTheme, userBarDispatch: dispatch }), _jsx("hr", {}), error && _jsx("b", { children: error }), _jsx(PostList, { posts: posts })] }) }));
+    // <HomePage posts={posts} dispatch={dispatch} error={error} />
+    return (_jsx(VDom.Context, { value: context, children: _jsxs("div", { style: { padding: 8 }, children: [_jsx(HeaderBar, { user: user, posts: posts, postDispatch: dispatch, setTheme: setTheme, userBarDispatch: dispatch }), _jsx("hr", {}), _jsx(PostPage, { id: 6 })] }) }));
 }
--- src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/App.tsx
+++ src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/App.tsx
@@ -1,19 +1,11 @@
 import { JSX, useReducer, useEffect, VDom, useState } from '@uif-js/core';
-// import UserBar from './user/UserBar';
-import PostList from './post/PostList';
-// import CreatePost from './post/CreatePost';
+// import PostList from './post/PostList';
 import appReducer from "./reducers";
-// import Header from './Header';
-// import ChangeTheme from './ChangeTheme';
 import { ThemeContext } from './contexts';
 import log from 'N/log';
-import query from "N/query";
 import HeaderBar from "./pages/HeaderBar";
-
-// const defaultPosts: IPostProps[] = [
-//   { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
-//   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
-// ];
+// import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
 
 export default function App(): JSX.Element {
   log.debug('App', `Initializing at ${new Date()}`);
@@ -21,24 +13,6 @@ export default function App(): JSX.Element {
   const [theme, setTheme] = useState({ primaryColor: 'deepskyblue', secondaryColor: 'coral' });
   const [state, dispatch] = useReducer(appReducer, { user: '', posts: [], error: '' });
   const { user, posts, error } = state;
-
-  const getPosts = () => { // Chapter 6_3: Doing this instead of useResource()
-    query.runSuiteQL.promise({ query: `SELECT id, name AS title, custrecord_blog_content AS content, BUILTIN.DF(owner) AS author FROM customrecord_blog_post` }).then((resultSet) => {
-      const results: { id: number, title: string, content: string, author: string }[] = resultSet.asMappedResults() as any;
-      dispatch({ type: 'FETCH_POSTS', posts: results });
-    });
-  }
-
-  useEffect(getPosts, []);
-  useEffect(() => {
-    if (posts && posts.error) {
-      dispatch({ type: 'POSTS_ERROR' });
-    }
-    if (posts && posts.data) {
-      dispatch({ type: 'FETCH_POSTS', posts: posts.data.reverse() });
-    }
-  }, [posts]);
-
 
   useEffect(() => {
     if (user) {
@@ -53,13 +27,13 @@ export default function App(): JSX.Element {
     [ThemeContext]: theme
   };
 
+  // <HomePage posts={posts} dispatch={dispatch} error={error} />
   return (
     <VDom.Context value={context}>
       <div style={{padding: 8}}>
         <HeaderBar user={user} posts={posts} postDispatch={dispatch} setTheme={setTheme} userBarDispatch={dispatch} />
         <hr/>
-        {error && <b>{error}</b>}
-        <PostList posts={posts} />
+        <PostPage id={6} />
       </div>
     </VDom.Context>
   )
