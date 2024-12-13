b4d01f4754f5b9e255d6a573f6cf285bf158b1d9
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2024-12-13 11:01:02 +0000

    Post: Got the Link to the post working

--- build/src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/post/Post.js
+++ build/src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/post/Post.js
@@ -1,16 +1,17 @@
 import { jsx as _jsx, jsxs as _jsxs } from "@uif-js/core/jsx-runtime";
-import { Route, useContext } from '@uif-js/core';
+import { useContext } from '@uif-js/core';
 import { ThemeContext } from '../contexts';
-import { RootRoute } from "../BlogAppRoute";
+// import {RootRoute} from "../BlogAppRoute";
 import { Link } from "@uif-js/component";
 export default function Post(props) {
     const { secondaryColor } = useContext(ThemeContext);
     let processedContent = props.content;
     if (props.short && props.content.length > 30)
         processedContent = props.content.substring(0, 30) + '...';
-    const blogPostRoute = Route.create(RootRoute.POST);
+    // const blogPostRoute = Route.create(RootRoute.POST);
     let linkToFullContent = null;
+    // if (props.short) linkToFullContent = <div><br/><Link url={blogPostRoute.constructUrl({ id: props.id })}>View full post</Link></div>
     if (props.short)
-        linkToFullContent = _jsxs("div", { children: [_jsx("br", {}), _jsx(Link, { url: blogPostRoute.constructUrl({ id: props.id }), children: "View full post" })] });
+        linkToFullContent = _jsxs("div", { children: [_jsx("br", {}), _jsx(Link, { route: { route: '/view/:id', parameters: { id: props.id } }, children: "View full post" })] });
     return (_jsxs("div", { children: [_jsx("h3", { style: { color: secondaryColor }, children: props.title }), _jsx("div", { children: processedContent }), linkToFullContent, _jsx("br", {}), _jsxs("i", { children: ["Written by ", _jsx("b", { children: props.author })] })] }));
 }
--- src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/post/Post.tsx
+++ src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/post/Post.tsx
@@ -1,15 +1,16 @@
-import { Route, useContext } from '@uif-js/core';
+import { useContext } from '@uif-js/core';
 import { ThemeContext } from '../contexts';
-import {RootRoute} from "../BlogAppRoute";
+// import {RootRoute} from "../BlogAppRoute";
 import {Link} from "@uif-js/component";
 
 export default function Post(props: IPostProps) {
   const {secondaryColor} = useContext(ThemeContext);
   let processedContent = props.content;
   if (props.short && props.content.length > 30) processedContent = props.content.substring(0, 30) + '...';
-  const blogPostRoute = Route.create(RootRoute.POST);
+  // const blogPostRoute = Route.create(RootRoute.POST);
   let linkToFullContent = null;
-  if (props.short) linkToFullContent = <div><br/><Link url={blogPostRoute.constructUrl({ id: props.id })}>View full post</Link></div>
+  // if (props.short) linkToFullContent = <div><br/><Link url={blogPostRoute.constructUrl({ id: props.id })}>View full post</Link></div>
+  if (props.short) linkToFullContent = <div><br/><Link route={{ route: '/view/:id', parameters: { id: props.id } }}>View full post</Link></div>
   return (
     <div>
       <h3 style={{color: secondaryColor}}>{props.title}</h3>
