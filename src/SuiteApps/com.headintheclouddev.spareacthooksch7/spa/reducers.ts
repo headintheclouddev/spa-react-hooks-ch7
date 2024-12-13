3767e85bcc9004558d79f24e5994085219a4781b
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2024-12-13 11:02:06 +0000

    Reducers: Store the ID with the new post

--- src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/reducers.ts
+++ src/SuiteApps/com.headintheclouddev.spareacthooksch7/spa/reducers.ts
@@ -13,13 +13,13 @@ function userReducer(state: string, action: { type?: string, username: string })
   }
 }
 
-function postsReducer(state: IPostProps[], action: { type?: string, title: string, content: string, author: string, posts: IPostProps[] }) {
+function postsReducer(state: IPostProps[], action: { type?: string, title: string, content: string, author: string, id: number, posts: IPostProps[] }) {
   console.log('postsReducer state', state);
   switch (action.type) {
     case 'FETCH_POSTS':
       return action.posts;
     case 'CREATE_POST':
-      const newPost = { title: action.title, content: action.content, author: action.author };
+      const newPost = { title: action.title, content: action.content, author: action.author, id: action.id };
       return [newPost, ...state];
     default:
       return state;
@@ -36,7 +36,7 @@ function errorReducer(state: string, action: { type?: string }) {
   }
 }
 
-export default function appReducer(state: { user: string, posts: IPostProps[], error: string }, action: { type?: string, title: string, content: string, author: string, username: string, posts: IPostProps[] }) {
+export default function appReducer(state: { user: string, posts: IPostProps[], error: string }, action: { type?: string, title: string, content: string, author: string, id: number, username: string, posts: IPostProps[] }) {
   console.log('appReducer state', state);
   return {
     user: userReducer(state.user, action),
